e94f39d6558f941e0c68489a1b4e6a3ac77bf046
Lisandro Mayancela <lmayancela08@gmail.com>
2017-06-30 18:01:51 +0000

    Finished Version

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
 /* global $ */
 
 $(document).ready(function() {
-    // Get the modal
+    
+});
+
+// Get the modal
     var modal = document.getElementById('myModal');
     
-    // Get the button that opens the modal
+// Get the button that opens the modal
     var btn = document.getElementById("sign-up");
     
-    // Get the <span> element that closes the modal
+// Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
     
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        var name = document.getElementById("name-sign").val();
-        var email = document.getElementById("email-sign").val();
-        document.getElementById("sign-up-modal").innerHTML = "Hi " + name + ", thank you for signing up! We will send an email to " + email + " shortly";
-        console.log("hi");
-        modal.style.display = "block";
-    }
+//Get the form
+    var form = document.getElementById("validate");
     
-    // When the user clicks on <span> (x), close the modal
+// When the user clicks the button, open the modal 
+    btn.onclick = function(e) {
+        var name = $("#name-sign").val();
+        var email = $("#email-sign").val();
+        var check = email.indexOf(".com");
+        if(name == ""||email == ""){
+            e.preventDefault();
+            document.getElementById("sign-up-modal").innerHTML = "Please make sure every field is filled out before submitting";
+            modal.style.display = "block";
+        }
+        else{
+            if(check == -1){
+                e.preventDefault();
+                document.getElementById("sign-up-modal").innerHTML = "Please enter a valid email";
+                modal.style.display = "block";
+            }
+            else{
+                e.preventDefault();
+                document.getElementById("sign-up-modal").innerHTML = "Hi " + name + " thank you for signing up! We will send an email to " + email + " shortly (Make sure popups are enabled for this page)";
+                modal.style.display = "block";
+                setTimeout(function(){
+                    form.target = "_blank";
+                    form.action = "https://formspree.io/markgriffith009@gmail.com";
+                    form.method = "POST";
+                    document.forms["validate"].submit();
+                },4000);
+            }
+            console.log(name);
+            console.log(email);
+            console.log(check);
+        }
+    };
+    
+// When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
-    }
+    };
     
-    // When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
-    }
-});
+    };
 
-
-$(".social-media-pic").hover(function() {
-    
-    $('i[class^="social-"]', this).addClass('icon-2x');
-}, function() {
-    $('i[class^="social-"]', this).removeClass('icon-2x');
-});
